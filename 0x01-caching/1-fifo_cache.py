e87b2b36fc2b1c2e31af81e80f2a81d82c18b8f7
Victoria-20 <victoriaocon111@gmail.com>
2023-01-27 11:08:02 +0300

    Fix issue

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,17 +7,26 @@ class FIFOCache(BaseCaching):
     """Implements the FIFO replacement policy"""
     def __init__(self) -> None:
         super().__init__()
+        self.keys_queue = []
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the
         item value for the key key"""
-        if key not in self.cache_data and item is not None:
+        if key in self.cache_data:
+            self.cache_data[key] = item
+        else:
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                self.cache_data.popitem()
-        print(f"DISCARD: {self.cache_data[key]}")
+                # remove old item from the cache
+                old_key = self.keys_queue.pop(0)
+                print(f"DISCARD: {old_key}", end='\n')
+                del self.cache_data[old_key]
+        # append the new item to tha cached_data
+        self.cache_data[key] = item
+        self.keys_queue.append(key)
 
     def get(self, key):
         """Returns the value in self.cache_data linked to the key """
-        if key is None or self.cache_data[key] is None:
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
             return None
-        return self.cache_data[key]
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,22 @@
+""" LIFOCache module """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Implements the LIFO replacement policy"""
+    def __init__(self) -> None:
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the
+        item value for the key key"""
+        if key not in self.cache_data and item is not None:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                self.cache_data.popitem()
+        print(f"DISCARD: {self.cache_data[key]}")
+
+    def get(self, key):
+        """Returns the value in self.cache_data linked to the key """
+        if key is None or self.cache_data[key] is None:
+            return None
+        return self.cache_data[key]
