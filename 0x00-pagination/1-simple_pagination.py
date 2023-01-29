e9aab7febf63055d3dccc113de7872726ed0b149
Victoria-20 <victoriaocon111@gmail.com>
2023-01-29 09:24:20 +0300

    1. Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,38 @@
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ takes two integer arguments page with default value 1 and
+        page_size with default value 10."""
+
+        self.dataset()
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        if len(self.__dataset) == 0:
+            return[]
+
+        start_index, end_index = index_range(page, page_size)
+        return(self.__dataset[start_index: end_index])
